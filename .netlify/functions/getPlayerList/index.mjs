9a1d2b96764f3938451013fab40185ec937d246a
ASKLL <askll520@outlook.com>
2025-01-02 20:08:45 +0800

    Revert "Player list fix"

--- .netlify/functions/getPlayerList/index.mjs
+++ .netlify/functions/getPlayerList/index.mjs
@@ -1,45 +1,32 @@
 import { MinecraftServerListPing } from 'minecraft-status';
-import axios from 'axios';
 
 const serverOptions = {
-  host: '101.42.137.83',
+  host: 'service.catpixel.cn',
   port: 10201,
   timeout: 3000,
 };
 
 exports.handler = async function(event, context) {
   try {
-    // 获取出口 IP 地址
-    const ipResponse = await axios.get('https://api.ipify.org?format=json');
-    const exitIp = ipResponse.data.ip;
-
-    // 获取 Minecraft 服务器状态
     const response = await MinecraftServerListPing.ping(4, serverOptions.host, serverOptions.port, serverOptions.timeout);
     const players = response.players;
     let message;
     if (players.online > 0) {
-      message = '';
-      players.sample.forEach((player, index) => {
-        message += `${player.name}`;
-        if (index < players.sample.length - 1) {
-          message += '、';
-        }
+      message = '当前在线玩家列表:\n';
+      players.sample.forEach(player => {
+        message += `${player.name}\n`;
       });
     } else {
       message = '暂无玩家在线';
     }
     return {
       statusCode: 200,
-      body: JSON.stringify({ message, exitIp }),
+      body: JSON.stringify({ message }),
     };
   } catch (error) {
-    // 获取出口 IP 地址，即使在发生错误时
-    const ipResponse = await axios.get('https://api.ipify.org?format=json');
-    const exitIp = ipResponse.data.ip;
-
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: '查询玩家列表出现错误: ' + error.message, exitIp }),
+      body: JSON.stringify({ error: '查询玩家列表出现错误: ' + error.message }),
     };
   }
 };
